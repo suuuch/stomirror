db18f51f432e9be92e6192e626fc4a86ac94d576
L S <L S>
2017-02-11 17:43:18 +0800

    封装网易财报获取

---
+++ From_WY/datafetch_163.py
@@ -0,0 +1,35 @@
+# encoding: utf-8
+
+from DataFecth.DataFetcher import FetcherDataFromUrl
+import pandas as pd
+import io
+
+class WYData(object):
+    def __init__(self):
+        self.reporttype = {
+            # 利润表
+            'lrb': {'url': 'http://quotes.money.163.com/service/lrb_%s.html'},
+            # 主要财务指标
+            'zycwzb': {'url': 'http://quotes.money.163.com/service/zycwzb_%s.html'},
+            # 资产负债表
+            'zcfzb': {'url': 'http://quotes.money.163.com/service/zcfzb_%s.html'},
+            # 财务报表摘要
+            'cwbbzy': {'url': 'http://quotes.money.163.com/service/cwbbzy_%s.html'},
+            # 盈利能力
+            'ylnl': {'url': 'http://quotes.money.163.com/service/zycwzb_%s.html?part=ylnl'},
+            # 偿还能力
+            'chnl': {'url': 'http://quotes.money.163.com/service/zycwzb_%s.html?part=chnl'},
+            # 成长能力
+            'cznl': {'url': 'http://quotes.money.163.com/service/zycwzb_%s.html?part=cznl'},
+            # 营运能力
+            'yynl': {'url': 'http://quotes.money.163.com/service/zycwzb_%s.html?part=yynl'},
+            # 现金流量表
+            'xjllb': {'url': 'http://quotes.money.163.com/service/xjllb_%s.html'}
+        }
+
+        self.fdf = FetcherDataFromUrl()
+
+    def get_report_type(self, rpt_type):
+        rpt_type_url = self.reporttype.get(rpt_type, None)
+        df = pd.read_csv(io.StringIO(self.fdf.download(rpt_type_url).content.decode('gb18030')))
+        return df.T
