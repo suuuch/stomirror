35059d1c24f2fb480cb9f29be902f8e5957c4073
L S <L S>
2017-03-14 22:00:58 +0800

    数据简单入库

--- From_WY/datafetch_163.py
+++ From_WY/datafetch_163.py
@@ -35,7 +35,8 @@ class WYData(object):
         assert rpt_type_url, 'Input Report Type : [%s]  NOT FOUND !' % rpt_type
         c = self.fdf.download(rpt_type_url['url'] % code)
         df = pd.read_csv(io.StringIO(c.content.decode('gb18030')))
-        df['report_date'] = df[df.columns[0]].apply(lambda x: '_'.join(lazy_pinyin(x)))
+        # df['type_name'] = df[df.columns[0]].apply(lambda x: '_'.join(lazy_pinyin(x)))
+        df['type_name'] = df[df.columns[0]]
         df.drop(df.columns[0], axis=1, inplace=True)
         df = df.convert_objects(convert_numeric=True)
-        return df[df.columns[~df.columns.str.contains('Unnamed')]].set_index('report_date').T
+        return df[df.columns[~df.columns.str.contains('Unnamed')]].set_index('type_name').T
